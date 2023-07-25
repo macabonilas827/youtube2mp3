6ba9c148a5664e0c94dc7904e2d7bd839d343da5
Mark Anel Cabonilas <macabonilas827@gmail.com>
2023-07-26 02:20:22 +0800

    add connection in client to server and vice versa communication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,45 @@ import { useState } from 'react'
 function App() {
 
   const [inputValue, setInputValue] = useState('')
+  const [returnData, setReturnData] = useState(null)
 
+  
   const regexPattern = /(?:[?&]v=|\/embed\/|\/1\/|\/v\/|https:\/\/(?:www\.)?youtu\.be\/)([^&\n?#]+)/
   const youtubeID = regexPattern.exec(inputValue)
-//  const {data} = useQuery({
-//   queryKey: ["data"],
-//   queryFn: async () => {
-//     const {data} = await axios.get('http://localhost:3000/api/v1/data')
-//     return data
-//   }
-//  })
+  
+  const mutation = useMutation({
+    mutationFn: (data) => {
+    return axios.post('http://127.0.0.1:3000/api/v1/data', data)
+  },
+  onSuccess: (data) => {
+    setReturnData(data)
+  }
+})
 
 
-console.log(youtubeID)
+const handleSubmit = e => {
+  e.preventDefault()
+
+  mutation.mutate({data: youtubeID[1]})
+  
+}
+let dataLink, dataTitle;
+if(returnData) {
+ const {data:{data:{link,title}}} = returnData
+dataLink = link
+
+dataTitle = title
+}
+
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="youtube url"   onChange={(e) => setInputValue(e.target.value) }  />
+      <button type="submit">Send it!</button>
+      </form>
+      <h6>{dataTitle}</h6>
+     <button><a href={dataLink}>Download</a></button> 
     </div>
   )
 }
--- server/app.js
+++ server/app.js
@@ -2,17 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const axios = require("axios");
 
 const port = process.env.PORT || 3000;
-
-const data = [{ id: 1, name: "Item 1" }];
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/v1/data", (req, res) => {
-  res.send(data);
+app.post("/api/v1/data", async (req, res) => {
+  const videoID = req.body.data;
+  const response = await axios.get(
+    `https://youtube-mp36.p.rapidapi.com/dl?id=${videoID}`,
+    {
+      headers: {
+        "X-RapidAPI-Key": process.env.API_KEY,
+        "X-RapidAPI-Host": process.env.API_HOST,
+      },
+    }
+  );
+  const data = response.data;
+
+  res.json({ success: true, data });
 });
 
 app.listen(port, function () {
