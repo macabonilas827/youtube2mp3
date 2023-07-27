c2125a3f1e27464105e6b5f153006cf0545f176b
Mark Anel Cabonilas <macabonilas827@gmail.com>
2023-07-27 23:58:34 +0800

    second pahse partial styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,20 +43,20 @@ dataTitle = title
   return (
     <div className="h-[40vh] bg-red-700 relative">
      
-      <form className="flex flex-col gap-2.5 w-4/6 h-[40vh] m-auto justify-end items-center pt-5 " onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-2.5 w-3/4 h-[40vh] m-auto justify-end items-center pt-5 " onSubmit={handleSubmit}>
         
-      <h1 className="absolute top-0 left-0 p-2 italic drop-shadow-sm text-4xl  font-black mb-20 ">YouTube2Mp3</h1>
-     <FaPlay className="absolute  top-5 h-28 w-28 z-1"  />
+      <h1 className="absolute top-0 left-0 p-2 italic drop-shadow-sm text-5xl  font-black mb-20 ">YouTube2Mp3</h1>
+     <FaPlay className="absolute  top-5 h-48 w-48 z-1"  />
       
-      <div className="flex justify-center items-center absolute -bottom-5">
-      <input className="w-72 p-3 rounded-l-2xl outline-none drop-shadow-md text-black text-xs" type="text" placeholder="youtube url"   onChange={(e) => setInputValue(e.target.value) }  />
-      <button className="p-3 rounded-r-2xl bg-slate-800 hover:bg-slate-900 drop-shadow-md text-white text-xs font-bold active:scale-x-105 transition-transform" type="submit">Convert</button>
+      <div className="flex justify-center w-3/4 items-center absolute -bottom-10">
+      <input className="w-9/12 p-7 rounded-l-3xl outline-none drop-shadow-md text-black text-md" type="text" placeholder="youtube url"   onChange={(e) => setInputValue(e.target.value) }  />
+      <button className="p-7 rounded-r-2xl bg-slate-800 hover:bg-slate-900 drop-shadow-md text-white text-md font-bold active:scale-x-105 transition-transform" type="submit">Convert</button>
       </div>
       </form>
 
-      <div className="flex flex-col justify-center items-center my-20">
+      <div className="flex flex-col justify-center items-center my-32">
       <h6>{dataTitle}</h6>
-     <button className=" btn py-1 px-3 rounded-full"><a href={dataLink}>Download</a></button> 
+     <button className=" btn py-5 px-16 rounded-full active:scale-x-90 transition-transform text-xl"><a href={dataLink}>Download</a></button> 
       </div>
     </div>
   )
